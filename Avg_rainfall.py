cc6df0eab8e5c2b603c2b82ea6ccf10b6e65cdb3
devpeterkim <devpeterkim@users.noreply.github.com>
2018-03-30 09:38:05 -0400

    Adding python tutorial. Average rainfall calculation
    Adding python tutorial.
    Average rainfall calculation

---
+++ Avg_rainfall.py
@@ -0,0 +1,54 @@
+#Calculate average rainfall
+
+january_to_june_rainfall = 1.93 + 0.71 + 3.53 + 3.41 + 3.69 + 4.50
+print january_to_june_rainfall
+
+annual_rainfall = january_to_june_rainfall
+
+
+july_rainfall = 1.05
+print july_rainfall
+
+annual_rainfall += july_rainfall
+print (" ")
+print annual_rainfall
+#Add July
+print ("Add July")
+
+august_rainfall = 4.91
+annual_rainfall += august_rainfall
+print (" ")
+print annual_rainfall
+#Add Aug
+print ("Add Aug")
+
+
+september_rainfall = 5.16
+annual_rainfall += september_rainfall
+print (" ")
+print annual_rainfall
+#Add Sept
+print ("Add Sept")
+
+october_rainfall = 7.20
+annual_rainfall += october_rainfall
+print (" ")
+print annual_rainfall
+#Add Oct
+print ("Add Oct")
+
+november_rainfall = 5.06
+annual_rainfall += november_rainfall
+print (" ")
+print annual_rainfall
+#Add Nov
+print ("Add Nov")
+
+
+december_rainfall = 4.06
+annual_rainfall += december_rainfall
+print (" ")
+print annual_rainfall
+#Add Dec
+print ("Add Dec")
+
